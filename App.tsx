2e3d0e37af305a5370b98ff5cf44d63f2bd62663
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2022-08-11 16:23:53 -0500

    tailwind implemented twrnc

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native';
-
+import tw from 'twrnc';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={tw``}>Open up App.tsx to start working on your app!</Text>
     </View>
   );
 }
