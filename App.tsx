25d734b3b693c496f8cc0d3722cc314e41336427
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2022-08-25 16:40:24 -0500

    working proyect

--- App.tsx
+++ App.tsx
@@ -1,17 +1,33 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Button, Image, Text, TouchableOpacity, View } from "react-native";
 import tw from "twrnc";
 
 export default function App() {
+  const [imageSwitch, setImageSwitch] = React.useState(false);
   return (
-    <View style={tw`flex-1 bg-white items-center justify-center items-center`}>
-      <View
-        style={tw`bg-blue-300 flex items-center py-[8px] rounded-md w-[90%] hover:bg-pink-400`}
+    <View
+      style={tw`flex flex-col h-[100%] w-[100%] bg-blue-300 items-center text-center  justify-center`}
+    >
+      <Image
+        source={{
+          uri: `${
+            imageSwitch
+              ? "https://i.ibb.co/k0hKzkr/pixels.png"
+              : "https://i.ibb.co/CmBkwQH/otronftqmeratie.png"
+          }`,
+        }}
+        style={tw.style("rounded-full mb-[5rem]", { width: 200, height: 200 })}
+      />
+      <Text style={tw`text-white font-bold text-[40px]`}>Hola mundo!</Text>
+      <Text style={tw`text-gray-900 font-thin mb-4`}>
+        Puro React Native gang
+      </Text>
+      <TouchableOpacity
+        onPress={() => setImageSwitch(!imageSwitch)}
+        style={tw.style("rounded-3xl bg-blue-200 px-4 py-2 ")}
       >
-        <Text style={tw`text-pink-400 font-bold text-2xl`}>
-          Que pedo perros
-        </Text>
-      </View>
+        <Text style={tw.style("text-xl font-bold text-white")}>Press me!</Text>
+      </TouchableOpacity>
     </View>
   );
 }
