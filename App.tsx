5257cc4524f994a868e6f86b2b2d222f06628e17
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2022-08-25 15:57:20 -0500

    expo running, changed yarn -> npm

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
+import React from "react";
 import { Text, View } from "react-native";
-import tw from "tailwind-react-native-classnames";
+import tw from "twrnc";
 
 export default function App() {
   return (
     <View style={tw`flex-1 bg-white items-center justify-center items-center`}>
-      <Text style={tw``}>Open up App.tsx to start working on your app!</Text>
+      <View
+        style={tw`bg-blue-300 flex items-center py-[8px] rounded-md w-[90%] hover:bg-pink-400`}
+      >
+        <Text style={tw`text-pink-400 font-bold text-2xl`}>
+          Que pedo perros
+        </Text>
+      </View>
     </View>
   );
 }
